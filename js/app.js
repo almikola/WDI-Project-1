a66ac67ee31467bec1f05d8ed38c704b306a862d
almikola <almikola@gmail.com>
2016-12-12 11:04:52 +0000

    figured out event listener

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ Game.gridHeight                 = 3;
 Game.width                      = 800;
 Game.height                     = 600;
 Game.sequenceT                  = [];
-Game.sequenceLengthTrump        = 14;
+Game.sequenceLengthTrump        = 9;
 Game.sequenceC                  = [];
 Game.sequenceLengthClinton      = 5;
 Game.sequenceO                  = [];
@@ -28,9 +28,10 @@ Game.createGrid = function () {
     square.style.height = (Game.height/Game.gridHeight) + 'px';
     grid.appendChild(square);
   }
+
   Game.SequenceTrump();
-  Game.SequenceClinton();
-  Game.SequenceObama();
+  // Game.SequenceClinton();
+  // Game.SequenceObama();
 };
 
 // TRUMP
@@ -39,7 +40,7 @@ Game.SequenceTrump = function() {
   for (var i = 0; i < Game.sequenceLengthTrump; i++) {
     Game.sequenceT.push(Game.randomSequenceNumberTrump());
   }
-  console.log(Game.sequenceT);
+
   Game.trump();
 };
 
@@ -50,60 +51,65 @@ Game.randomSequenceNumberTrump = function() {
 Game.trump = function() {
   var squares = document.getElementsByTagName('li');
   for (var i = 0; i < Game.sequenceT.length; i++) {
-    squares[Game.sequenceT[i]].className = 'trump';
-    Game.pointsT();
+    squares[Game.sequenceT[i]].setAttribute('class', 'trump');
+  }
+  Game.pointsT();
+};
+
+Game.pointsT = function(){
+  var pointsT = document.getElementsByClassName('trump');
+  for (var i = 0; i < pointsT.length; i++) {
+    pointsT[i].addEventListener('click', Game.earnPointsT);
   }
 };
 
-Game.pointsT = function() {
-  this.addEventListener('click', earnPointsT());
-  earnPointsT = function () {
-    console.log('getPoints');};
-    };
-
-// CLINTON
-
-Game.SequenceClinton = function() {
-  for (var i = 0; i < Game.sequenceLengthClinton; i++) {
-    Game.sequenceC.push(Game.randomSequenceNumberClinton());
-  }
-  console.log(Game.sequenceC);
-  Game.clinton();
+Game.earnPointsT = function () {
+  
 };
 
-Game.randomSequenceNumberClinton = function() {
-  return Math.round(Math.random() * (Game.gridBase * Game.gridHeight));
-};
-
-Game.clinton = function() {
-  var squares = document.getElementsByTagName('li');
-  for (var i = 0; i < Game.sequenceC.length; i++) {
-    squares[Game.sequenceC[i]].className = 'clinton';
-  }
-  // Game.pointsC();
-};
-
-// OBAMA
-
-Game.SequenceObama = function() {
-  for (var i = 0; i < Game.sequenceLengthObama; i++) {
-    Game.sequenceO.push(Game.randomSequenceNumberObama());
-  }
-  console.log(Game.sequenceO);
-  Game.obama();
-};
-
-Game.randomSequenceNumberObama = function() {
-  return Math.round(Math.random() * (Game.gridBase * Game.gridHeight));
-};
-
-Game.obama = function() {
-  var squares = document.getElementsByTagName('li');
-  for (var i = 0; i < Game.sequenceO.length; i++) {
-    squares[Game.sequenceO[i]].className = 'obama';
-  }
-  // Game.pointsO();
-};
+// // CLINTON
+//
+// Game.SequenceClinton = function() {
+//   for (var i = 0; i < Game.sequenceLengthClinton; i++) {
+//     Game.sequenceC.push(Game.randomSequenceNumberClinton());
+//   }
+//   console.log(Game.sequenceC);
+//   Game.clinton();
+// };
+//
+// Game.randomSequenceNumberClinton = function() {
+//   return Math.round(Math.random() * (Game.gridBase * Game.gridHeight));
+// };
+//
+// Game.clinton = function() {
+//   var squares = document.getElementsByTagName('li');
+//   for (var i = 0; i < Game.sequenceC.length; i++) {
+//     squares[Game.sequenceC[i]].className = 'clinton';
+//   }
+//   // Game.pointsC();
+// };
+//
+// // OBAMA
+//
+// Game.SequenceObama = function() {
+//   for (var i = 0; i < Game.sequenceLengthObama; i++) {
+//     Game.sequenceO.push(Game.randomSequenceNumberObama());
+//   }
+//   console.log(Game.sequenceO);
+//   Game.obama();
+// };
+//
+// Game.randomSequenceNumberObama = function() {
+//   return Math.round(Math.random() * (Game.gridBase * Game.gridHeight));
+// };
+//
+// Game.obama = function() {
+//   var squares = document.getElementsByTagName('li');
+//   for (var i = 0; i < Game.sequenceO.length; i++) {
+//     squares[Game.sequenceO[i]].className = 'obama';
+//   }
+//   // Game.pointsO();
+// };
 
 document.addEventListener('DOMContentLoaded', Game.start);
 // find out why putting this at the top prevents it from running correctly
